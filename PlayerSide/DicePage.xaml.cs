1ebd24c5dd9c9418b6142d1f897206cd0d40fa56
Skidi <skidi007@gmail.com>
2022-09-17 00:55:14 +0200

    MAUI: Dice tab implementation

--- PlayerSide/DicePage.xaml.cs
+++ PlayerSide/DicePage.xaml.cs
@@ -1,9 +1,93 @@
+using SharedClassLibrary;
+using System;
+
 namespace PlayerSide;
 
 public partial class DicePage : ContentPage
 {
-	public DicePage()
-	{
-		InitializeComponent();
-	}
+    private D20 _d20;
+    public DicePage()
+    {
+        InitializeComponent();
+        InitializeDice();
+    }
+
+    private void OnRollClicked(object sender, EventArgs e)
+    {
+        RollBtn.IsEnabled = false;
+        DiceRolls.RelRotateTo(360, (uint)_d20.RollingDuration.TotalMilliseconds, Easing.CubicOut);
+        _d20.Roll();
+    }
+
+    private void InitializeDice()
+    {
+        _d20 = new D20();
+        _d20.RollingChanged += OnDiceRollingChanged;
+        _d20.Rolled += OnDiceRolled;
+    }
+
+    void OnDiceRolled(object sender, EventArgs e)
+    {
+        RollBtn.IsEnabled = true;
+    }
+
+    void OnDiceRollingChanged(object sender, EventArgs e)
+    {
+        DiceRolls.Source = _d20.ImageName();
+    }
+
+    private void OnAdd8BtnClicked(object sender, EventArgs e)
+    {
+        RollBtn.IsEnabled = false;
+        DiceRolls.RelRotateTo(360, (uint)_d20.RollingDuration.TotalMilliseconds, Easing.CubicOut);
+        _d20.Roll();
+    }
+    private void OnAdd4BtnClicked(object sender, EventArgs e)
+    {
+        RollBtn.IsEnabled = false;
+        DiceRolls.RelRotateTo(360, (uint)_d20.RollingDuration.TotalMilliseconds, Easing.CubicOut);
+        _d20.Roll();
+    }
+    private void OnAdd10BtnClicked(object sender, EventArgs e)
+    {
+        RollBtn.IsEnabled = false;
+        DiceRolls.RelRotateTo(360, (uint)_d20.RollingDuration.TotalMilliseconds, Easing.CubicOut);
+        _d20.Roll();
+    }
+    private void OnAdd00BtnClicked(object sender, EventArgs e)
+    {
+        RollBtn.IsEnabled = false;
+        DiceRolls.RelRotateTo(360, (uint)_d20.RollingDuration.TotalMilliseconds, Easing.CubicOut);
+        _d20.Roll();
+    }
+    private void OnAdd6BtnClicked(object sender, EventArgs e)
+    {
+        RollBtn.IsEnabled = false;
+        DiceRolls.RelRotateTo(360, (uint)_d20.RollingDuration.TotalMilliseconds, Easing.CubicOut);
+        _d20.Roll();
+    }
+    private void OnAdd12BtnClicked(object sender, EventArgs e)
+    {
+        RollBtn.IsEnabled = false;
+        DiceRolls.RelRotateTo(360, (uint)_d20.RollingDuration.TotalMilliseconds, Easing.CubicOut);
+        _d20.Roll();
+    }
+    private void OnAdd20BtnClicked(object sender, EventArgs e)
+    {
+        RollBtn.IsEnabled = false;
+        DiceRolls.RelRotateTo(360, (uint)_d20.RollingDuration.TotalMilliseconds, Easing.CubicOut);
+        _d20.Roll();
+    }
+    private void OnRemoveDiceBtnClicked(object sender, EventArgs e)
+    {
+        RollBtn.IsEnabled = false;
+        DiceRolls.RelRotateTo(360, (uint)_d20.RollingDuration.TotalMilliseconds, Easing.CubicOut);
+        _d20.Roll();
+    }
+    private void OnClearDiceBtnClicked(object sender, EventArgs e)
+    {
+        RollBtn.IsEnabled = false;
+        DiceRolls.RelRotateTo(360, (uint)_d20.RollingDuration.TotalMilliseconds, Easing.CubicOut);
+        _d20.Roll();
+    }
 }
