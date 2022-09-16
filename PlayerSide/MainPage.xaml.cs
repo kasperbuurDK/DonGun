cc6b3360379c4b5d6026ae7157b5de64db72e12a
Skidi <skidi007@gmail.com>
2022-09-16 21:36:05 +0200

    MAUI: Tabbed pages

--- PlayerSide/MainPage.xaml.cs
+++ PlayerSide/MainPage.xaml.cs
@@ -1,5 +1,4 @@
 ﻿namespace PlayerSide;
-using SharedClassLibrary;
 
 public partial class MainPage : ContentPage
 {
@@ -12,60 +11,9 @@ public partial class MainPage : ContentPage
      *      Add number of dices selection option.
      *      
      */
-
-    private D20 _d20;
-    private float _count;
-
-    public float Count
-    {
-        set
-        {
-            if (value < 0)
-                _count = 0;
-            else
-                _count = value;
-        }
-        get { return _count; }
-    }
     public MainPage()
-	{
-		InitializeComponent();
-        InitializeDice();
-    }
-
-	private void OnCounterPosClicked(object sender, EventArgs e)
-	{
-        Count += 0.1F;
-        CharPc.UpdateBars(Count, Count);
-    }
-
-    private void OnCounterNegClicked(object sender, EventArgs e)
     {
-        Count -= 0.1F;
-        CharPc.UpdateBars(Count, Count);
-    }
-    private void OnRollClicked(object sender, EventArgs e)
-    {
-        RollBtn.IsEnabled = false;
-        DiceRolls.RelRotateTo(360, (uint)_d20.RollingDuration.TotalMilliseconds, Easing.CubicOut);
-        _d20.Roll();
-    }
-
-    private void InitializeDice()
-    {
-        _d20 = new D20();
-        _d20.RollingChanged += OnDiceRollingChanged;
-        _d20.Rolled += OnDiceRolled;
-    }
-
-    void OnDiceRolled(object sender, EventArgs e)
-    {
-        RollBtn.IsEnabled = true;
-    }
-
-    void OnDiceRollingChanged(object sender, EventArgs e)
-    {
-        DiceRolls.Source = _d20.ImageName();
+        InitializeComponent();
     }
 }
 
