2c241c6f56ac86fbb89c667cea2e76c45b48255a
Kasper Buur Vistesen <kasperbuur@gmail.com>
2022-09-18 11:43:04 +0200

    tried deleting donSide again

--- DonWeb/Models/CurrentGame.cs
+++ DonWeb/Models/CurrentGame.cs
@@ -5,14 +5,14 @@
        // [Key]
        public int Id { get; set; }
        public string Name { get; set; }
-       public int[] ActivePLayers { get; set; }
+       public Array ActivePLayers { get; set; }
        public int CurrentTurn { get; set; }
+        public string GameType { get; set; }
 
 
         public CurrentGame()
         {
-
+        
         }
-
     }
 }
