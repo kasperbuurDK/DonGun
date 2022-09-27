8381075ad23bbfe2713fe23e97e666e71f0ec5ca
Kasper Buur at Rokoko <kasper.buur@rokoko.com>
2022-09-27 23:44:47 +0200

    added testProject

---
+++ DonGunTest/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace DonGunTest
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
