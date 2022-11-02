4c6bbe782a401f99e3a7581e5856453aafc48b0d
kasperBuurDk <kasperbuur@gmail.com>
2022-11-02 16:51:11 +0100

    added offensive action

---
+++ SharedClassLibrary/AuxUtils/GameMasterHelpers.cs
@@ -0,0 +1,19 @@
+﻿namespace SharedClassLibrary.AuxUtils
+{
+    public class GameMasterHelpers
+    {
+        public static float DetermineDistanceBetweenCharacters(Character character, Character otherCharacter)
+        {
+            float deltaX =
+                Math.Max(character.Position.X, otherCharacter.Position.X) - Math.Min(character.Position.X, otherCharacter.Position.X);
+            float deltaY =
+                Math.Max(character.Position.Y, otherCharacter.Position.Y) - Math.Min(character.Position.Y, otherCharacter.Position.Y);
+
+            if (deltaX == 0) return deltaY;
+            if (deltaY == 0) return deltaX;
+
+            float distanceBetween = (float)Math.Sqrt(Math.Pow(deltaX, 2) + Math.Pow(deltaY, 2));
+            return distanceBetween;
+        }
+    }
+}
--- SharedClassLibrary/GameMaster.cs
+++ SharedClassLibrary/GameMaster.cs
@@ -6,12 +6,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using SharedClassLibrary;
 
 namespace SharedClassLibrary
 {
 
-    public class GameMaster
+    public class GameMaster : GameMasterHelpers
     {
         private Game _game;
 
@@ -74,20 +73,6 @@ namespace SharedClassLibrary
 
             character.OthersInSight = othersInSight;
             character.UpdatePossibleActions();
-        }
-
-        private float DetermineDistanceBetweenCharacters(Character character, Character otherCharacter)
-        {
-            float deltaX = 
-                Math.Max(character.Position.X, otherCharacter.Position.X) - Math.Min(character.Position.X, otherCharacter.Position.X);
-            float deltaY = 
-                Math.Max(character.Position.Y, otherCharacter.Position.Y) - Math.Min(character.Position.Y, otherCharacter.Position.Y);
-
-            if (deltaX == 0) return deltaY;
-            if (deltaY == 0) return deltaX;
-
-            float distanceBetween = (float) Math.Sqrt(Math.Pow(deltaX, 2) + Math.Pow(deltaY, 2));
-            return distanceBetween;
         }
 
         private int CostOfTurningCharacter(Character character, MoveDirections turnTowards)
